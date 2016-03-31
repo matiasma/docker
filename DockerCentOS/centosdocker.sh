1b5130bc916904860e428392e89f03af2343e0fe
matiasma <matias_marcelo@live.com>
2016-03-30 21:11:02 -0300

    versao inicial

---
+++ DockerCentOS/centosdocker.sh
@@ -0,0 +1,16 @@
+﻿#!/bin/sh
+
+date > /tmp/azuredeploy.log.$$ 2>&1
+whoami >> /tmp/azuredeploy.log.$$ 2>&1
+echo $@ >> /tmp/azuredeploy.log.$$ 2>&1
+
+sudo yum -y update >> /tmp/azuredeploy.log.$$ 2>&1
+
+curl -fsSL https://get.docker.com/ | sh >> /tmp/azuredeploy.log.$$ 2>&1
+
+sudo chkconfig docker on >> /tmp/azuredeploy.log.$$ 2>&1
+
+sudo service docker start >> /tmp/azuredeploy.log.$$ 2>&1
+
+exit 0
+
