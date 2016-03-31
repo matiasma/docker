7c05760bbbd8ec9d62f640f92abe20fad6cb51b6
matiasma <matias_marcelo@live.com>
2016-03-30 21:27:24 -0300

    Update centosdocker.sh

--- DockerCentOS/centosdocker.sh
+++ DockerCentOS/centosdocker.sh
@@ -2,15 +2,12 @@
 
 date > /tmp/azuredeploy.log.$$ 2>&1
 whoami >> /tmp/azuredeploy.log.$$ 2>&1
-echo $@ >> /tmp/azuredeploy.log.$$ 2>&1
 
-sudo yum -y update >> /tmp/azuredeploy.log.$$ 2>&1
-
+yum -y update >> /tmp/azuredeploy.log.$$ 2>&1
 curl -fsSL https://get.docker.com/ | sh >> /tmp/azuredeploy.log.$$ 2>&1
-
-sudo chkconfig docker on >> /tmp/azuredeploy.log.$$ 2>&1
-
-sudo service docker start >> /tmp/azuredeploy.log.$$ 2>&1
+systemctl enable docker >> /tmp/azuredeploy.log.$$ 2>&1
+systemctl start docker >> /tmp/azuredeploy.log.$$ 2>&1
+systemctl status docker >> /tmp/azuredeploy.log.$$ 2>&1
 
 exit 0
 
